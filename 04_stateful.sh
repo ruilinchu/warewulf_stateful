c0426193166ce012ecdb18c34987a4a91d281359
ruilinchu <ruilinchu@gmail.com>
2015-09-15 15:09:31 -0700

    update

--- 04_stateful.sh
+++ 04_stateful.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 yum -y --installroot=/var/chroots/centos-6-stateful install kernel grub
-
-cp -f /etc/passwd /var/chroots/centos-6-stateful/etc
-cp -f /etc/group /var/chroots/centos-6-stateful/etc
-cp -f /etc/shadow /var/chroots/centos-6-stateful/etc
-
 wwvnfs -y --chroot /var/chroots/centos-6-stateful
 
 ./ww-configure-stateful.sh c001 500 sda
