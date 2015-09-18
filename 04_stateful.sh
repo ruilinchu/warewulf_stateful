417a980fd570fe40373fb90f173f20a997005c02
ruilinchu <ruilinchu@gmail.com>
2015-09-18 16:36:08 -0700

    update

--- 04_stateful.sh
+++ 04_stateful.sh
@@ -6,6 +6,8 @@ cp -f /etc/passwd  /var/chroots/centos-6-stateful/etc
 cp -f /etc/group  /var/chroots/centos-6-stateful/etc
 cp -f /etc/shadow  /var/chroots/centos-6-stateful/etc
 
+cp -fr /etc/pki/rpm-gpg/ /var/chroots/centos-6-stateful/etc/pki/
+
 wwvnfs -y --chroot /var/chroots/centos-6-stateful
 
 ./ww-configure-stateful.sh c[001-004] 500 sda
